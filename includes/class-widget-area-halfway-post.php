20f110b8bcf9a32323d84576d2b2149c51ae89f1
Dan0sz <18595395+Dan0sz@users.noreply.github.com>
2020-08-28 23:34:44 +0200

    v1.0.0

---
+++ includes/class-widget-area-halfway-post.php
@@ -0,0 +1,87 @@
+<?php
+/* * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * @package  : dan0sz/widget-area-halfway-post
+ * @author   : Daan van den Bergh
+ * @copyright: (c) 2020 Daan van den Bergh
+ * @url      : https://daan.dev | https://woosh.dev
+ * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+class WidgetAreaHalfwayPost
+{
+    /** @var string $handle */
+    private $handle = 'widget-area-halfway-post';
+
+    /** @var string $plugin_text_domain */
+    private $plugin_text_domain = 'widget-area-halfway-post';
+
+    /**
+     * WidgetAreaHalfwayPostGp constructor.
+     */
+    public function __construct()
+    {
+        $this->init();
+    }
+
+    /**
+     * Hook into required actions.
+     */
+    private function init()
+    {
+        add_action('widgets_init', [$this, 'register_sidebar'], 0, 100);
+        add_action('wp', [$this, 'maybe_insert_sidebar'], 0, 200);
+    }
+
+    /**
+     * Should we insert the widget area on this page?
+     */
+    public function maybe_insert_sidebar()
+    {
+        if (!is_admin()
+            && is_single()
+            && get_post_type() == 'post'
+        ) {
+            wp_enqueue_style($this->handle, plugin_dir_url(DAAN_WIDGET_AREA_HALFWAY_POST_PLUGIN_FILE) . 'assets/css/widget-area-halfway-post.min.css', ['generate-style' ], DAAN_WIDGET_AREA_HALFWAY_POST_STATIC_VERSION);
+            add_filter('the_content', [$this, 'insert_sidebar']);
+        }
+    }
+
+    /**
+     * Register the widget area in WP Admin.
+     */
+    public function register_sidebar()
+    {
+        register_sidebar(
+            [
+                'id'          => 'halfway-post',
+                'name'        => __('Halfway Post Content', $this->plugin_text_domain),
+                'description' => __('Insert widgets halfway the post\'s content.', $this->plugin_text_domain)
+            ]
+        );
+    }
+
+    /**
+     * @param $html
+     *
+     * @return string
+     */
+    public function insert_sidebar($html)
+    {
+        $headers = preg_split('@(?=\<h2\>)@', $html);
+        $middle  = (int) ceil(count($headers) / 2);
+
+        ob_start();
+        ?>
+        <div class="halfway-post widget-area">
+        <?php
+        dynamic_sidebar('halfway-post');
+        ?>
+        </div>
+        <?php
+        $sidebar = ob_get_contents();
+        ob_end_clean();
+
+        array_splice($headers, $middle, 0, $sidebar);
+
+        return implode('', $headers);
+    }
+}
---
+++ widget-area-halfway-post.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * @formatter:off
+ * Plugin Name: Widget Area Halfway Post
+ * Plugin URI: https://github.com/Dan0sz/widget-area-halfway-post
+ * Description: Add a widget area before the middle chapter of your post's content.
+ * Version: 1.0.0
+ * Author: Daan van den Bergh
+ * Author URI: https://daan.dev
+ * Text Domain: widget-area-halfway-post-gp
+ * Github Plugin URI: Dan0sz/widget-area-halfway-post
+ * @formatter:on
+ */
+
+defined('ABSPATH') || exit;
+
+/**
+ * Define constants.
+ */
+define('DAAN_WIDGET_AREA_HALFWAY_POST_PLUGIN_DIR', plugin_dir_path(__FILE__));
+define('DAAN_WIDGET_AREA_HALFWAY_POST_PLUGIN_FILE', __FILE__);
+define('DAAN_WIDGET_AREA_HALFWAY_POST_PLUGIN_BASENAME', plugin_basename(__FILE__));
+define('DAAN_WIDGET_AREA_HALFWAY_POST_STATIC_VERSION', '1.0.0');
+
+/**
+ * Takes care of loading classes on demand.
+ *
+ * @param $class
+ *
+ * @return mixed|void
+ */
+function daan_widget_area_halfway_post_autoload($class)
+{
+    $path = explode('_', $class);
+
+    if ($path[0] != 'WidgetAreaHalfwayPost') {
+        return;
+    }
+
+    if (!class_exists('Woosh_Autoloader')) {
+        require_once(DAAN_WIDGET_AREA_HALFWAY_POST_PLUGIN_DIR . 'woosh-autoload.php');
+    }
+
+    $autoload = new Woosh_Autoloader($class);
+
+    return include DAAN_WIDGET_AREA_HALFWAY_POST_PLUGIN_DIR . 'includes/' . $autoload->load();
+}
+
+spl_autoload_register('daan_widget_area_halfway_post_autoload');
+
+/**
+ * @return WidgetAreaHalfwayPost
+ */
+function woosh_widget_area_halfway_post()
+{
+    static $widget_area_halfway_post = null;
+
+    if ($widget_area_halfway_post === null) {
+        $widget_area_halfway_post = new WidgetAreaHalfwayPost();
+    }
+
+    return $widget_area_halfway_post;
+}
+
+woosh_widget_area_halfway_post();
